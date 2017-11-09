52eaff1f96e1a702dda896d3fe3b8817b7ab6993
Dániel Kántor <git@daniel-kantor.com>
2017-11-09 19:19:33 +0100

    Added slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 function create_vis() {
     var width = document.getElementById("network").clientWidth
     var height = 500
@@ -16,6 +15,16 @@ function create_simulation() {
         .force("center", d3.forceCenter(width / 2, height / 2))
 }
 
+function organize_by_date(items) {
+    var table = {}
+
+    items.map((item) => {table[item["year"]] = {}})
+    items.map((item) => {table[item["year"]][item["month"]] = []})
+    items.map((item) => {table[item["year"]][item["month"]].push(item)})
+
+    return table
+}
+
 function load_data(simulation) {
     d3.csv("edges.csv", function(error, edges) {
         if (error) throw error;
@@ -25,18 +34,33 @@ function load_data(simulation) {
             return {"id": d, "group": d % 3}
         })
 
+        var frames = [
+           [2017, 1], [2017, 2], [2017, 3], [2017, 4], [2017, 5], [2017, 6], [2017, 7], [2017, 8], [2017, 9], [2017, 10], [2017, 11], [2017, 12],
+           [2018, 1], [2018, 2], [2018, 3], [2018, 4], [2018, 5], [2018, 6], [2018, 7], [2018, 8], [2018, 9], [2018, 10], [2018, 11], [2018, 12],
+           [2019, 1], [2019, 2], [2019, 3], [2019, 4], [2019, 5], [2019, 6], [2019, 7], [2019, 8], [2019, 9], [2019, 10], [2019, 11], [2019, 12],
+        ]
+
+        d3.select("#timeline")
+            .attr("value", 0)
+            .attr("min", 0)
+            .attr("max", frames.length - 1)
+
         var edges = edges.map((d) => {
             return {
                 "source": d["from"],
                 "target": d["to"],
+                "year": d["year"],
+                "month": d["month"],
                 "weight": d["weight"] / 50,
             }
         })
+        
+        var organized_edges = organize_by_date(edges)
 
         var link = d3.select("svg").append("g")
             .attr("class", "links")
             .selectAll("line")
-            .data(edges)
+            .data(organized_edges[2017][2])
             .enter().append("line")
             .attr("stroke-width", function(d) { return Math.sqrt(d.weight); });
 
@@ -53,10 +77,14 @@ function load_data(simulation) {
             .on("tick", ticked)
 
         simulation.force("link")
-            .links(edges)
+            .links(organized_edges[2017][2])
 
         resize()
         d3.select(window).on("resize", resize)
+        d3.select("#timeline").on("change", () => {
+            var item = document.getElementById("timeline")
+            load_frame(frames[item.value])
+        })
 
         function ticked() {
             link
@@ -84,6 +112,24 @@ function load_data(simulation) {
                     .y(height / 2)
 
             simulation
+                .alpha(1)
+                .restart()
+        }
+
+        function load_frame(item) {
+            var year = item[0],
+                month = item[1]
+            var data = organized_edges[year][month]
+
+            var link = d3.select("svg g")
+                .selectAll("line")
+                .data(data)
+
+            simulation.force("link")
+                .links(data)
+
+            simulation
+                .alpha(1)
                 .restart()
         }
 
