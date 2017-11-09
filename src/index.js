d1730eb59e7084739edf3afe06acc060c85dbd09
Dániel Kántor <git@daniel-kantor.com>
2017-11-09 13:24:32 +0100

    layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("hello world")
+function create_vis() {
+    return d3.select("#network").append("svg").attr("width", 600).attr("height", 400)
+}
+
+$(document).ready(() => {
+    create_vis()
+});
